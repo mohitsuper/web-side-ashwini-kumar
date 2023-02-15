82ad8710f6f82d8ec0ec0f299ac269636673a41f
Animemuically6@gamil.com <Animemuically6@gamil.com>
2023-02-15 18:24:06 +0530

    web-side-ashwini-kumar

---
+++ main.js
@@ -0,0 +1,15 @@
+
+  let search = document.querySelector('.searchbox');
+  
+  let search_open = document.querySelector('.search');
+  
+  let search_close = document
+  .querySelector('.close');
+  
+  search_close.style.display = 'none';
+  
+  search.onclick = function()
+  {
+    searchbox.classList.add('active');
+  }
+  
