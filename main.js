925dd5ab0ab4cf6d970799b5a7fbe57b4170ede5
animemuically6@gamil.com <animemuically6@gamil.com>
2023-03-24 22:30:31 +0530

    web-side-ashwini-kumar

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ console.log('Hello World!');
 var sidemeu = document.getElementById('sidemenu');
   
   function openemenu(){
-    sidemenu.style.right ='0';
+    sidemenu.style.right ='-100%';
   }
   
   function closemenu(){
-        sidemenu.style.right ='-200px';
+        sidemenu.style.right ='0px';
 
   }
 
