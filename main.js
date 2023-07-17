de37a323e6af51c0311878b1024613e708e2eb92
Mohit vijay <124493371+mohitsuper@users.noreply.github.com>
2023-07-17 04:20:47 -0700

    Update main.js
    Signed-off-by: Mohit vijay <124493371+mohitsuper@users.noreply.github.com>

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ console.log('Hello World!');
 var sidemeu = document.getElementById('sidemenu');
   
   function openemenu(){
-    sidemenu.style.right ='-100%';
+    sidemenu.style.right ='0';
   }
   
   function closemenu(){
-        sidemenu.style.right ='0px';
+        sidemenu.style.right ='-200px';
 
   }
   
