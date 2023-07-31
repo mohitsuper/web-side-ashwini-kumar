895226774792a18f87c5dc1e9e5bb82baccf6da3
animemusically6@gmail.com <animemusically6@gmail.com>
2023-07-31 22:27:11 +0530

    we

--- main.js
+++ main.js
@@ -5,14 +5,17 @@
 console.log('Hello World!');
 var sidemeu = document.getElementById('sidemenu');
   
-  function openemenu(){
-    sidemenu.style.right ='0';
+
+  
+    function openemenu(){
+    sidemenu.style.right ='100%';
   }
   
   function closemenu(){
-        sidemenu.style.right ='-200px';
+        sidemenu.style.right ='0px';
 
   }
+  
   
   var mainImg = document.getElementById('mainImg');
   
